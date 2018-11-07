a5bcbe0f7fb20a9784ec155591d76601c3d95258
Chris Simpkins <git.simpkins@gmail.com>
2018-11-07 10:37:28 -0500

    [lib.rs] add has_args, has_options, contains_arg, contains_option methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! `commandlines` is a simple, functional command line argument 
+//! parsing library for the development of Rust command line interface 
+//! (CLI) applications.
+
 mod parsers;
 
 use std::fmt;
@@ -6,7 +10,7 @@ use std::fmt;
 pub struct Command {
     pub argv: Vec<String>,
     pub argc: usize,
-    pub options: Vec<String>
+    pub options: Vec<String>,
 }
 
 // Traits
@@ -44,8 +48,24 @@ impl Command {
         Command {
             argv: arguments,
             argc: vec_size,
-            options: vec_options
+            options: vec_options,
         }
+    }
+
+    pub fn has_args(&self) -> bool {
+        self.argv[1..].len() > 0
+    }
+
+    pub fn has_options(&self) -> bool {
+        self.options.len() > 0
+    }
+
+    pub fn contains_arg(&self, needle: &str) -> bool {
+        self.argv[1..].contains(&String::from(needle))
+    }
+
+    pub fn contains_option(&self, needle: &str) -> bool {
+        self.options.contains(&String::from(needle))
     }
 }
 
@@ -71,4 +91,57 @@ mod tests {
         let c = Command::new(vec!["test".to_string(), "--help".to_string()]);
         assert!(c.argc == 2);
     }
+
+    #[test]
+    fn command_method_has_args_true() {
+        let c = Command::new(vec!["test".to_string(), "--help".to_string()]);
+        assert_eq!(c.has_args(), true);
+
+        let c = Command::new(vec!["test".to_string(), "subcmd".to_string()]);
+        assert_eq!(c.has_args(), true);
+    }
+
+    #[test]
+    fn command_method_has_args_false() {
+        let c = Command::new(vec!["test".to_string()]); // ignores the executable as not an argument
+        assert_eq!(c.has_args(), false);
+    }
+
+    #[test]
+    fn command_method_has_options_true() {
+        let c = Command::new(vec!["test".to_string(), "--help".to_string()]);
+        assert!(c.has_options() == true);
+    }
+
+    #[test]
+    fn command_method_has_options_false() {
+        let c = Command::new(vec!["test".to_string(), "subcmd".to_string()]);
+        assert!(c.has_options() == false);
+    }
+
+    #[test]
+    fn command_method_contains_arg() {
+        let c = Command::new(vec![
+            "test".to_string(),
+            "subcmd".to_string(),
+            "--help".to_string(),
+        ]);
+        assert_eq!(c.contains_arg("subcmd"), true);
+        assert_eq!(c.contains_arg("--help"), true);
+        assert_eq!(c.contains_arg("bogus"), false);
+        assert_eq!(c.contains_arg("test"), false); // executable not considered argument
+    }
+
+    #[test]
+    fn command_method_contains_option() {
+        let c = Command::new(vec![
+            "test".to_string(),
+            "subcmd".to_string(),
+            "--help".to_string(),
+        ]);
+        assert_eq!(c.contains_option("--help"), true);
+        assert_eq!(c.contains_option("--bogus"), false);
+        assert_eq!(c.contains_option("help"), false); // must include the option indicator in string
+    }
+
 }
